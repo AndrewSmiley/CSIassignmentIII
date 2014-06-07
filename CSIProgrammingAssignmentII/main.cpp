f7dbfc13c0d65c311590ce1b3d3204a6438afad2
Andrew Smiley <andrew.smiley@live.com>
2014-06-07 11:48:35 -0400

    Upload

---
+++ CSIProgrammingAssignmentII/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  CSIProgrammingAssignmentII
+//
+//  Created by Andrew Smiley on 6/6/14.
+//  Copyright (c) 2014 Andrew Smiley. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    //loop for filing until they decide otherwise
+    bool shouldEnd = false;
+    while(!shouldEnd){
+        //greeting
+        cout << "Welcome!"<<endl;
+        cout << "Today we will calculate taxes based upon rates in 2002."<<endl;
+        
+        int filingType;
+        cout << "Please choose from one of the following options: " << endl<<endl;
+        cout << "1. Single Filers"<<endl<<"2. Married Filing Jointly"<<endl<<"3. Married Filing Separately"<<endl<<"4. Head of Household";
+        
+        
+        
+    }
+    
+}
+
